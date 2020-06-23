5af524f25b22568dd247c6a8cc6c6f45a127e382
Sabine <sabine@21.lv>
2020-06-23 21:57:35 +0200

    added .env, procfile and reqirements

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipedb'
+app.config['MONGO_URI'] = os.getenv(
+    'MONGO_URI')
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'hola'
+@app.route('/get_category')
+def get_categories():
+    return render_template("category.html")
 
 
+# categories=mongo.db.categories.find()
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
             port=int(os.getenv("PORT", "5000")), debug=True)
