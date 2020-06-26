2c289efc7fe7d969b2c4122d7ae2be72bccabcd4
Sabine <sabine@21.lv>
2020-06-27 00:12:56 +0200

    added recipe page and test data

--- app.py
+++ app.py
@@ -22,6 +22,12 @@ def get_categories():
     return render_template("category.html", categories=categories_cursor)
 
 
+@app.route('/recipe')
+def get_recipe():
+    recipe_cursor = mongo.db.recipe.find()
+    return render_template("recipe.html", recipe=recipe_cursor)
+
+
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
             port=int(os.getenv("PORT", "5000")), debug=True)
